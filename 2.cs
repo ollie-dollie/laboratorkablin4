470c4b501779a38d19f2d2217e60a5dc717fa713
ollie-dollie <olandreeva01p@mail.ru>
2025-09-16 15:00:19 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,10 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int p = 1;
+for (int i = 1; i <= m; i++){
+    p *= a * (a - i * n);
+}
+Console.WriteLine(p);
+
+
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int p = 1;
+int i = 1;
+while (i <= m)
+{
+    p *= a * (a - i * n);
+    i++; 
+}
+Console.WriteLine(p);
